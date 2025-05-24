84089a02c38b440f08b85be5c0b79d9bec29133a
RoshanGorakhpuriya <roshangorakhpuriya@gmail.com>
2025-05-24 21:17:30 +0530

    Testimonial Added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeatureDestination from '../components/FeatureDestination'
 import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero/>
       <FeatureDestination/>
       <ExclusiveOffer/>
+      <Testimonial/>
+      <NewsLetter/>
     </>
   )
 }
