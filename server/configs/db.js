ca523dd1a6ffb52404eff1a45c1085bb0f7ea12d
RoshanGorakhpuriya <roshangorakhpuriya@gmail.com>
2025-05-27 17:54:29 +0530

    Update All file for testing

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,34 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async ()=>{
+//     try{
+//         mongoose.connection.on('connected', ()=>console.log("Database Connected"));
+
+
+//         await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+//     } catch(error){
+//         console.log(error.message);
+//     }
+// }
+
+// export default connectDB
+
+// configs/db.js
+
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try{
-        mongoose.connection.on('connected', ()=>console.log("Database Connected"));
+const dbConnect = async () => {
+  if (mongoose.connections[0].readyState) return;
 
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Error:", error.message);
+  }
+};
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
-    } catch(error){
-        console.log(error.message);
-    }
-}
-
-export default connectDB
+export default dbConnect;
