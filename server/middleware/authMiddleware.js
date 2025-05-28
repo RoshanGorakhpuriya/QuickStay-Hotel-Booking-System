bd8ccdd0d710c5e53e5253be1f89a9e987f340a1
RoshanGorakhpuriya <roshangorakhpuriya@gmail.com>
2025-05-28 14:39:24 +0530

    Create API for Booking , hotel

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+// import User from "../models/User.js"
+
+// export const protect = async(req , res , next)=>{
+//     const {userId} = req.auth;
+
+//     if(!userId){
+//         res.json({success:false , message :"not authenticated"})
+//     }
+
+//     else{
+//         const user = await User.findById(userId);
+
+//         req.user = user;
+//         next()
+//     }
+// }
+
+
+// server/middleware/authMiddleware.js
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  const { userId } = req.auth;
+
+  if (!userId) {
+    return res.status(401).json({ success: false, message: "not authenticated" });
+  }
+
+  const user = await User.findById(userId);
+  req.user = user;
+  next();
+};
+
